c72628eb17a76560ebccd3ca54608da4839317ce
User <kigradomir@gmail.com>
2022-02-23 18:40:02 +0330

    hw4

--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from movie_app.models import Director, Movie, Review
+from rest_framework.exceptions import ValidationError
 
 class DirectorsSerializers(serializers.ModelSerializer):
     class Meta:
@@ -27,5 +28,29 @@ class MoviesSerializers(serializers.ModelSerializer):
         serializer = ReviewSerializer(Review.objects.filter(movie=movie), many=True)
         return serializer.data
 
+class DirectorValidateSerializers(serializers.Serializer):
+    name = serializers.CharField(min_length=10, max_length=50)
+
+class MovieCreateValidateSerializers(serializers.Serializer):
+    title = serializers.CharField(min_length=2, max_length=10)
+    description = serializers.CharField()
+    duration = serializers.IntegerField()
+    director = serializers.IntegerField()
+
+
+    def validate_director_id(self, director_id):
+        if Director.objects.filter(id=director_id).count()==0:
+            raise ValidationError(f"Director with id = {director_id} not found")
+        return director_id
+
+class ReviewCreateValidateSerializers(serializers.Serializer):
+    text = serializers.CharField(max_length=50)
+    movie = serializers.IntegerField()
+    stars = serializers.IntegerField(min_value=1, max_value=5)
+
+    def validate_movie_id(self, movie_id):
+        if Movie.objects.filter(id=movie_id).count()==0:
+            raise ValidationError(f"Movie with id = {movie_id} not found")
+        return movie_id
 
 
